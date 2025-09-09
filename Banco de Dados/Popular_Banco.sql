0316c84d22fb07a215c209f6afde15a5bf7b58c3
ALUNO SESI SENAI <ALUNO SESI SENAI@SSIRT01LABF20>
2025-09-09 08:57:17 -0300

    Calcular Metragem v0.1

--- Banco de Dados/Popular_Banco.sql
+++ Banco de Dados/Popular_Banco.sql
@@ -1,8 +1,8 @@
 USE estoque_rolos;
 
 INSERT INTO rolos (Code, Descricao, Milimetragem, MOQ, Estoque, MetragemWIP) VALUES
-(789634, 'Bobina Pequena', 25, 15.000, 10.000, 5.000),
-(789587, 'Bobina Média', 35, 15.000, 12.000, 3.000),
-(789344, 'Bobina Grande', 50, 15.000, 15.000, 000),
-(789124, 'Bobina Pequena', 10, 15.000, 8.000, 7.000),
-(789896, 'Bobina Grande', 70, 15.000, 13.000, 2.000);
+('P125', 'Bobina Pequena', 80, 15.000, 10.000, 5.000),
+('P160', 'Bobina Média', 100, 15.000, 12.000, 3.000),
+('P200', 'Bobina Grande', 125, 15.000, 15.000, 000),
+('P250', 'Bobina Pequena', 160, 15.000, 8.000, 7.000),
+('P355', 'Bobina Grande', 225, 15.000, 13.000, 2.000);
